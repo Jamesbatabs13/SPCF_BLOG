e06dcdbc5b47c597651702e2148aab2ca4b15029
JamesCoder14 <jamesbatayolaspcf@gmail.com>
2022-04-08 07:43:13 +0800

    SPCF_PROJECT

---
+++ resources/views/index.blade.php
@@ -0,0 +1,102 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="background-image grid grid-cols-1 m-auto">
+        <div class="flex text-gray-100 pt-10">
+            <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
+                <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
+                    Do you want to become a student developer at System PLus College Foundation?
+                </h1>
+                <a 
+                    href="/blog"
+                    class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
+                    Read More
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+        <div>
+            <img src="https://cdn.pixabay.com/photo/2016/11/18/18/37/programming-1836330_960_720.png" width="700" alt="">
+        </div>
+
+        <div class="m-auto sm:m-auto text-left w-4/5 block">
+            <h2 class="text-3xl font-extrabold text-gray-600">
+                Struggling to be a better  Student Web developer?
+            </h2>
+            
+            <p class="py-8 text-gray-500 text-s">
+            Join our students and express your thoughts on web development and other topics!.
+            </p>
+
+            <p class="font-extrabold text-gray-600 text-s pb-9">
+            Create a free account and start sharing right away.
+            </p>
+
+            <a 
+                href="/blog"
+                class="uppercase bg-blue-500 text-gray-100 text-s font-extrabold py-3 px-8 rounded-3xl">
+                Find Out More
+            </a>
+        </div>
+    </div>
+
+    <div class="text-center p-15 bg-black text-white">
+        <h2 class="text-2xl pb-5 text-l"> 
+            I'm a Student  and an expert in...
+        </h2>
+
+        <span class="font-extrabold block text-4xl py-1">
+            PHP
+        </span>
+        <span class="font-extrabold block text-4xl py-1">
+            Project Management
+        </span>
+        <span class="font-extrabold block text-4xl py-1">
+            Digital Strategy
+        </span>
+        <span class="font-extrabold block text-4xl py-1">
+            Backend Development
+        </span>
+    </div>
+
+    <div class="text-center py-15">
+        <span class="uppercase text-s text-gray-400">
+            Students Recent Post
+        </span>
+
+        <h2 class="text-4xl font-bold py-10">
+            Recent Posts
+        </h2>
+
+        <p class="m-auto w-4/5 text-gray-500">
+            Start testing your Laravel application today
+        </p>
+    </div>
+
+    <div class="sm:grid grid-cols-2 w-4/5 m-auto">
+        <div class="flex bg-yellow-700 text-gray-100 pt-10">
+            <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block">
+                <span class="uppercase text-xs">
+                    PHP
+                </span>
+
+                <h3 class="text-xl font-bold py-10">
+                    What is Laravel? Explain it like I'm five.
+                    So you've kind of heard about Laravel. You're maybe considering learning it, but there are just too many things to take into account. Tinker? Artisan? Composer? Why does everything sound like character classes in an RPG game?
+                    This is by far no definitive guide, but if you've ever asked yourself “why should I bother with all this stuff”, read on for some of the most asked
+                </h3>
+
+                <a 
+                    href=""
+                    class="uppercase bg-transparent border-2 border-gray-100 text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
+                    Find Out More here!
+                </a>
+            </div>
+        </div>
+        <div>
+            <img src="https://cdn.pixabay.com/photo/2015/01/08/18/11/laptops-593296_960_720.jpg" alt="">
+        </div>
+    </div>
+@endsection
